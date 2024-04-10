8b94fed3729e1ae379eb401443e3223815cc2dd1
Rooban <roobanstark@gmail.com>
2024-04-10 13:15:28 +0530

    Add project files.

---
+++ zamazon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace zamazon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
